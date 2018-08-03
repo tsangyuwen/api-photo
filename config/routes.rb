263c75de308c99319a8094229462285eaaeee74b
tsang <B10423013@gemail.yuntech.edu.tw>
2018-08-03 20:02:29 +0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "photos#index"
+  resources :photos
+  resources :users
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :photos, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
+
+end
